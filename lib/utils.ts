4cd3cba25c9f000c7415b64a55b93ab57f891ee5
nabathnm <muhammadnabathnuur@gmail.com>
2026-06-16 22:58:44 +0700

    feat: create utils supabase

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function toTitleCase(str: string): string {
+  if (!str) return str;
+  return str.replace(
+    /\w\S*/g,
+    (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  );
+}
