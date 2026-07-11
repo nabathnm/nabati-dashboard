ac55bf82f717bc7b689e8ecd6ea495b44841c67c
nabathnm <muhammadnabathnuur@gmail.com>
2026-07-11 21:44:34 +0700

    commit

--- components/dashboard/accounts-overview.tsx
+++ components/dashboard/accounts-overview.tsx
@@ -52,7 +52,7 @@ export default function AccountsOverview() {
           ))
         ) : !activeAccounts.length ? (
           <div className="py-8 text-center text-sm text-muted-foreground">
-            No accounts yetouhhouohg
+            No accounts yet
           </div>
         ) : (
           activeAccounts.map((account) => {
