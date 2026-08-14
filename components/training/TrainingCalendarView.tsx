aaf0e7fd87854698a6268aafedf24c188a3eb021
nabathnm <muhammadnabathnuur@gmail.com>
2026-08-14 22:52:15 +0700

    fix error

--- components/training/TrainingCalendarView.tsx
+++ components/training/TrainingCalendarView.tsx
@@ -16,7 +16,7 @@ export default function TrainingCalendarView() {
 
   // Fetch trainings for the current month
   const { data: trainings = [], isLoading } = useTrainings({
-    startDate: format(startOfMonth(currentDate), "yyyy-MM-dd"),
+    startDate: format(startOfMonth(currentDate), "yyyy-MM-dddddd"),
     endDate: format(endOfMonth(currentDate), "yyyy-MM-dd"),
   });
 
