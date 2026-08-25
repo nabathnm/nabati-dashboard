574505afd303139f0dc3190bcda99b493d18acdc
nabathnm <muhammadnabathnuur@gmail.com>
2026-08-25 22:34:25 +0700

    c

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -25,7 +25,7 @@ export default function DashboardLayout({
           setIsMobileOpen={setIsMobileOpen}
         />
 
-        {/* Main Content Area aaaadsasddsasadsadsadasdassaaaaaaaaaaaaaaaaadsadsssaaaaaaaaaaaaaaaasasa*/}
+        {/* Main Content Area*/}
         <main
           className={cn(
             "flex-1 transition-all duration-300 ease-in-out pt-16 sm:pt-0",
